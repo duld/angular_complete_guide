bf169fe2cbc1ddbf9c69698b59e1d15acbf873a5
Jeffrey McCrea <duldren@gmail.com>
2019-03-27 15:58:38 -0700

    Add simple binding to first-app

--- projects/01-first-app/first-app/src/app/servers/servers.component.ts
+++ projects/01-first-app/first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  buttonDisabled = false;
+  
+  constructor() {
+    setTimeout(() => {
+      this.buttonDisabled = true;
+    }, 2500);
+  }
 
   ngOnInit() {
+
   }
 
 }
