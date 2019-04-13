53953b33ec9114e5ca6d797f5da3fa303fca3bc9
Jeffrey McCrea <duldren@gmail.com>
2019-04-13 12:41:16 -0700

    Add starting components project 2

--- projects/02-basics-project/basics-project/src/app/app.module.ts
+++ projects/02-basics-project/basics-project/src/app/app.module.ts
@@ -1,11 +1,29 @@
+// angular //
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// app //
 import { AppComponent } from './app.component';
+// header  //
+import { HeaderComponent } from './header/header.component';
+// recipe  //
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+// shopping list  //
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
