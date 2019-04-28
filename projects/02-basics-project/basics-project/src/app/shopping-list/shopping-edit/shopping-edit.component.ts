1a00740e0cb4bb18edc4f10da86919b9e4002d44
Jeffrey McCrea <duldren@gmail.com>
2019-04-28 12:56:47 -0700

    Add basic new ingredient form submit

--- projects/02-basics-project/basics-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/02-basics-project/basics-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addIngredient: EventEmitter<Ingredient> = new EventEmitter();
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmitIngredient() {
+    console.log('addIngredient!')
+    const newIngredient = new Ingredient(
+      this.nameInput.nativeElement.value, 
+      this.amountInput.nativeElement.value);
+    this.addIngredient.emit(newIngredient);
+  }
 }
--- projects/02-basics-project/basics-project/src/app/shopping-list/shopping-list.component.ts
+++ projects/02-basics-project/basics-project/src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,8 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  
+  
   ingredients: Ingredient[] = [
     new Ingredient('Whole Chicken', 1),
     new Ingredient('thyme', 2)
@@ -16,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onAddIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
