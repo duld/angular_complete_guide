24617a056c299c229b491456324d7866b1097a18
Jeffrey McCrea <duldren@gmail.com>
2019-04-27 17:30:30 -0700

    Add app selection to project 2

--- projects/02-basics-project/basics-project/src/app/app.component.ts
+++ projects/02-basics-project/basics-project/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appToDisplay: string;
+
+  onShowRecipes(str) {
+    this.appToDisplay = str;
+  }
+
+  onShowShoppingList(str) {
+    this.appToDisplay = str;
+  }
+
+  onAppSelect(str) {
+    this.appToDisplay = str;
+  }
 }
