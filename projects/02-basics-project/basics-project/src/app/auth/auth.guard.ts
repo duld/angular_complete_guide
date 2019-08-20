2cbb89e454b7b786983fd631e687aaf46b310ae3
Jeffrey McCrea <duldren@gmail.com>
2019-08-20 16:50:25 -0700

    Add  auth guard

---
+++ projects/02-basics-project/basics-project/src/app/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { take, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      //
+      return this.authService.user.pipe(
+        take(1),
+        map( userInfo => {
+          if (userInfo) {
+            return true;
+          }
+          return this.router.createUrlTree(['/auth']);
+        })
+      );
+  }
+}
